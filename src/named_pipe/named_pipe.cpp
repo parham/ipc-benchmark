0f3e8042810eeba21b048d2bb7c769d58f4151c9
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-08-29 22:15:12 -0400

    implement named pipe

---
+++ src/named_pipe/named_pipe.cpp
@@ -0,0 +1,46 @@
+
+#include "named_pipe.h"
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+NamedPipe::~NamedPipe() {
+    // Empty body
+}
+
+retcode_ NamedPipe::initializeImpl_() {
+    if (this->getMode() == Server) {
+        int fid = mkfifo(pipPath.c_str(), 0666);
+        if (fid < 0) {
+            return FIFO_CREATE_FAILED;
+        }
+    }
+
+    this->pipFID = open(this->pipPath.c_str(), this->getMode() == IPCMode::Server ? O_WRONLY : O_RDONLY);
+    return SUCCESS_RET;
+}
+
+retcode_ NamedPipe::terminateImpl_() {
+    close(this->pipFID);
+    if (this->getMode() == Server) {
+        remove(pipPath.c_str());
+    }
+    return SUCCESS_RET;
+}
+
+retcode_ NamedPipe::updateImpl_(DataPtr_ data) {
+    int ret = write(this->pipFID, data, this->containerSize);
+    if (ret < 0) {
+        return WRITING_FAILED;
+    }
+    return SUCCESS_RET;
+}
+
+retcode_ NamedPipe::getImpl_(DataPtr_ data) {
+    int ret = read(this->pipFID, data, this->containerSize);
+    if (ret < 0) {
+        return READING_FAILED;
+    }
+    return SUCCESS_RET;
+}
---
+++ src/named_pipe/named_pipe.h
@@ -0,0 +1,37 @@
+//
+// Created by phm on 25/08/23.
+//
+
+#ifndef IPC_BENCHMARK_NAMED_PIPE_H
+#define IPC_BENCHMARK_NAMED_PIPE_H
+
+#include <filesystem>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <common/core.h>
+
+namespace fs = std::filesystem;
+
+class NamedPipe : public AbstractIPC {
+private:
+    size_d containerSize;
+    fs::path pipPath;
+    int pipFID;
+
+public:
+    NamedPipe(std::string name, IPCMode mode, size_d csize) : AbstractIPC(name, mode), containerSize(csize) {
+        fs::path dir (NAMED_PIPE_DIR);
+        fs::path file (this->getName());
+        this->pipPath = dir / file;
+    }
+    ~NamedPipe();
+
+protected:
+    retcode_ initializeImpl_() override;
+    retcode_ terminateImpl_() override;
+    retcode_ updateImpl_(DataPtr_ data) override;
+    retcode_ getImpl_(DataPtr_ data) override;
+};
+
+#endif //IPC_BENCHMARK_NAMED_PIPE_H
